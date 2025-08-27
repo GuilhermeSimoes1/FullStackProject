ce60ae5e53ddadd8c077b53908bf19500eac8624
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-08-27 23:05:00 +0100

    Set up database with EF Core and seed initial Movies data

---
+++ Api/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Data
+
+{
+    using Api.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ Api/Data/DataSeeder.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Data
+{
+    using Api.Models;
+    using Newtonsoft.Json;
+    public static class DataSeeder
+    {
+        public static void Seed(AppDbContext context)
+        {
+            if(!context.Movies.Any())
+            {
+                var json = File.ReadAllText("movies.json");
+                var movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+
+                context.Movies.AddRange(movies!);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
