fd878133bb0d5135a1c82d848f6c7062de255d10
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-08-29 16:43:07 +0100

    Set up client connection to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5107/api/movies")
+      .then (response => {
+        setMovies(response.data);
+        console.log(response.data);
+      })
+      .catch (error => {
+        console.error("There was an error fetching the movies!", error);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de Filmes</h1>
+      <ul>
+        {movies.map((movie, index) => (
+        <li key={index}> 
+          {JSON.stringify(movie)}
+        </li>
+        )
+        )}  
+      </ul>
     </div>
+
   );
 }
 
