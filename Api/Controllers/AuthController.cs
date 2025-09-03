1542139750ecc2e1684f554da63b46d7e36a913a
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-09-03 22:42:11 +0100

    add AuthController with register and login endpoints

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Api.Data
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        
+        public AuthController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+        {
+            //Fazer lógica
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            // Fazer lógica
+        }
+    }
+}
